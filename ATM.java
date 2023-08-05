62d52985eb5d5ead7505d5366091771edd400bd8
Rahul Kumar <140732259+Rahul6Kumar@users.noreply.github.com>
2023-08-05 15:08:32 +0530

    Task-4 ATM INTERFACE

---
+++ ATM.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class ATM extends AtmOptionMenu {
+    public static void main(String[] args) throws IOException {
+        AtmOptionMenu optionMenu = new AtmOptionMenu();
+        optionMenu.getLogin();
+    }
+
+}
